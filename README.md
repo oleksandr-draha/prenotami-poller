d06ef15643218575f248aa77692cb4f909a4ba91
Oleksandr Draha <oleksandr.draha@takeoff.com>
2022-09-21 17:48:33 +0300

    Almost done

--- README.md
+++ README.md
@@ -5,7 +5,10 @@
 
  `pip install -r requirements.txt`
 
-3. Edit file **main.py** with specifying your desired login/password/number of service to click
+3. OPTIONAL Edit file **main.py** specifying your desired login/password/number of service to click
 4. Run script and wait to dates to be available
 
  `python main.py`
+ 
+5. Also you can specify user email, password and number of service to book:
+    `python main.py email@i.ua passWORD 2`
--- main.py
+++ main.py
@@ -1,16 +1,36 @@
+import sys
+
 from selenium import webdriver
 from selenium.common import NoAlertPresentException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from webdrivermanager.chrome import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 URL = "https://prenotami.esteri.it/"
-LOGIN = "cappajet@gmail.com"
-PASSWORD = "Yo89kaT7"
+LOGIN = ""
+PASSWORD = ""
 SERVICES_URL = "https://prenotami.esteri.it/Services"
-BOOK_NUMBER = 4
+BOOK_NUMBER = 0
 CHECK_TIMEOUT = 2
+
+if not LOGIN:
+    try:
+        LOGIN = sys.argv[1]
+    except IndexError:
+        LOGIN = input("User email: ")
+
+if not PASSWORD:
+    try:
+        PASSWORD = sys.argv[2]
+    except IndexError:
+        PASSWORD = input("User password: ")
+
+if not BOOK_NUMBER:
+    try:
+        BOOK_NUMBER = int(sys.argv[3])
+    except IndexError:
+        BOOK_NUMBER = int(input("Number of service to book: "))
 
 LOGIN_LOCATOR = (By.ID, "login-email")
 PASSWORD_LOCATOR = (By.ID, "login-password")
@@ -19,24 +39,29 @@ NOT_AVAILABLE_LOCATOR = (By.XPATH, "//*[contains(text(),'Al momento non ci sono 
 PRIVACY_CHECK_BOX_LOCATOR = (By.XPATH, "//input[@type='checkbox']")
 FORWARD_BUTTON_LOCATOR = (By.ID, "btnAvanti")
 OK_POPUP_LOCATOR = (By.ID, "//*[contains(text(),'OK')]")
+AVAILABLE_DAY = (By.XPATH, "//td[@class='day availableDay']")
+SUBMIT_BUTTON = (By.ID, "btnPrenotaNoOtp")
 
 
 # Setup Chrome
-ChromeDriverManager().download_and_install()
+service = ChromeDriverManager().install()
 caps = DesiredCapabilities().CHROME
 caps["pageLoadStrategy"] = "eager"
-driver = webdriver.Chrome(desired_capabilities=caps)
+driver = webdriver.Chrome(service, desired_capabilities=caps)
 driver.implicitly_wait(10)
 
 
 # Login to portal
+print("Getting portal")
 driver.get(URL)
+print("Logging with provided credentials")
 driver.find_element(*LOGIN_LOCATOR).send_keys(LOGIN)
 driver.find_element(*PASSWORD_LOCATOR).send_keys(PASSWORD)
 driver.find_element(*PASSWORD_LOCATOR).submit()
 
 
 # Extract service link
+print("Getting services list")
 driver.get(SERVICES_URL)
 service = driver.find_elements(*BOOK_LOCATOR)[BOOK_NUMBER - 1]
 service_link = service.get_attribute("href")
@@ -50,22 +75,39 @@ def check_availability():
     privacy_checkbox = driver.find_elements(*PRIVACY_CHECK_BOX_LOCATOR)
     if len(privacy_checkbox):
         # If check box is present - then click it
+        print("Service is available for booking. Trying to book it")
         privacy_checkbox[0].click()
         # And click FORWARD button
         driver.find_element(*FORWARD_BUTTON_LOCATOR).click()
         return True
 
 
-def wait_for_alert():
-    try:
-        driver.switch_to.alert.accept()
-        driver.switch_to.window(0)
-        return True
-    except NoAlertPresentException:
-        return False
 # Constantly polling for dates available
+print("Starting to wait for dates available")
 while not check_availability():
     driver.get(service_link)
 
+
+def wait_for_alert():
+    try:
+        driver.switch_to.alert.accept()
+        driver.switch_to.default_content()
+        return True
+    except NoAlertPresentException:
+        return False
+
+
+# Waiting for alert to appear
+print("Waiting for confirmation shown")
 while not wait_for_alert():
     pass
+
+
+# Selecting first available day and submit
+print("Selecting first available day")
+driver.find_element(*AVAILABLE_DAY).click()
+print("Confirming booking")
+driver.find_element(*SUBMIT_BUTTON).click()
+
+print("Booking was finished")
+input("Press Enter when finished")
